dc104c9c83bd50b95b8eeb2670fe63717ac3431c
Thiago-Reis-Porto <42750555+Thiago-Reis-Porto@users.noreply.github.com>
2021-12-11 00:32:53 -0300

    Update fnUseds.py

--- fnUseds.py
+++ fnUseds.py
@@ -274,7 +274,7 @@ def get_losses_and_lr(opt, scheduler, epochs_losses, batches_losses):
 
 def batch_interaction(model, loss_fn, opt, data, batches_losses, no_val=False, preds=None, targets=None):
     for xb, yb in data:
-        pred = model(xb)
+        pred = model(xb.to('cuda'))
         loss = loss_fn(pred, yb)
         loss.backward()
         opt.step()
